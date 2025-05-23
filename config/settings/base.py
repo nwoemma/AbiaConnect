86ef5dac52179305c81e7f19e9f418081cda1a9d
unknown <nwosuemmanuel159@gmail.com>
2025-05-23 23:07:16 +0100

    Latest 5

--- config/settings/base.py
+++ config/settings/base.py
@@ -137,10 +137,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 AUTH_USER_MODEL = 'accounts.User'
-
-
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 # LOGIN_REDIRECT_URL = 'accounts:dashboard/'
