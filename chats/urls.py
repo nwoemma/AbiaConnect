a52388cdaa42cf490d220a3be23bf13c19149449
unknown <nwosuemmanuel159@gmail.com>
2025-05-24 00:43:17 +0100

    Latest 10

--- chats/urls.py
+++ chats/urls.py
@@ -1,8 +1,8 @@
 # urls.py
 from django.urls import path
-from .views import home_view  # adjust the import path based on where you put the view
+from .views import home_chat # adjust the import path based on where you put the view
 
 urlpatterns = [
-    path('', home_view, name='home'),
+    path('', home_chat, name='home'),
     # ... other paths
 ]
