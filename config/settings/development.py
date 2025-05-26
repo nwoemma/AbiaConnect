258a4ee9257942985fe8ed5309c12c8a00bfb5f0
unknown <nwosuemmanuel159@gmail.com>
2025-05-26 18:10:01 +0100

    change development database

--- config/settings/development.py
+++ config/settings/development.py
@@ -10,11 +10,18 @@ ALLOWED_HOSTS = ['127.0.0.1', '10.0.2.1']
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'abia2025',
+        'USER': 'abia_user',
+        'PASSWORD': 'BYABPZ9xPxY7U3rsamblVnOtnyKSEao9',
+        'HOST': 'dpg-d0otn1ruibrs7384a9og-a.frankfurt-postgres.render.com',
+        'PORT': '5432',
+        # Add this line if your host requires SSL
+        'OPTIONS': {
+            'sslmode': 'require',
+        },
     }
 }
-
 #STATIC_ROOT = BASE_DIR/ 'static'
 
 
