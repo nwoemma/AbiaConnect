d7e4b1567cee5a9de1b2abfd00a8c09f0317eb79
unknown <nwosuemmanuel159@gmail.com>
2025-06-05 11:11:10 +0100

    Fix schema view by removing invalid permission_classes

--- social/urls.py
+++ social/urls.py
@@ -33,7 +33,7 @@ urlpatterns = [
     # path('', include('chats.urls')),
     # path('accounts/', include('accounts.urls', namespace='accounts')),
 
-    path('api/schema/', SpectacularAPIView.as_view(permission_classes=[]), name='schema'),
+    path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
     path('api/docs/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
     path('redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
     
